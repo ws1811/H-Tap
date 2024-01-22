ff183d41c498c1aa3a6e0812392ba9b0c68192a3
wonsun <98735772+ws1811@users.noreply.github.com>
2024-01-22 09:59:45 +0900

    commit before pull

--- app/src/main/java/kr/co/htap/MainActivity.kt
+++ app/src/main/java/kr/co/htap/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* 온보딩 페이지 */
+        // 1. 슬라이드용 화면 준비
+
     }
 }
